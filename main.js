562ee2d78f66738f7dbcfa3146c70d0de61d683a
sabrina palm <sabrina_palm@hotmail.com>
2018-02-15 20:07:21 +0100

    update

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-const database = firebase.database();
+    
 let githubLogin = document.getElementById('githubLogin');
+let loggedIn = document..getElementById('logged-in');
+
+
+
+
 
 let provider = new firebase.auth.GithubAuthProvider();
 
 
-
-
-
 // Logga in den autentiserade användaren
 githubLogin.addEventListener('click', function(event){
+    github();
+})
+
+
+//Autentiserad login
+function github() {
     firebase.auth().signInWithPopup(provider)
     .then(function(result) {
-	// Om autentisering lyckas, så finns användarinfo i user
 	let user = result.user;
         console.log(user);
     });
-})
-    
-    
+}
 
-    
+
+
+
+
+
+
+
 // Logga ut den autentiserade användaren
 firebase.auth().signOut()
 .then(function(result) {
@@ -32,6 +42,23 @@ firebase.auth().signOut()
 });
 
     
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
