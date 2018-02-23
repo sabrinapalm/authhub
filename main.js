c9e84a227e197d3d25e16b30faf6ca9ec039913f
sabrina palm <sabrina_palm@hotmail.com>
2018-02-23 19:01:33 +0100

    added new functions and css

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
+
 let githubLogin = document.getElementById('githubLogin');
 let loggedIn = document.getElementById('logged-in');
-
-
-
-
+let card = document.getElementById('card');
+let logout;
 
 let provider = new firebase.auth.GithubAuthProvider();
 
@@ -20,9 +18,62 @@ githubLogin.addEventListener('click', function(event){
 function github() {
     firebase.auth().signInWithPopup(provider)
     .then(function(result) {
-	let user = result.user;
+	     let user = result.user;
+
+       //hide githubLogin
+       githubLogin.style.display = 'none';
         console.log(user);
-    });
+
+        //userinfo
+        let name = user.displayName;
+        let email = user.email;
+        let photo = user.photoURL;
+
+        //create elements
+        let h1 = document.createElement('h1');
+        let p = document.createElement('p');
+        let logout = document.createElement('button');
+        let img = document.createElement('img');
+
+        //apply info to elements
+        img.src = photo;
+        img.alt = name;
+        h1.innerText = name;
+        p.innerText = email;
+        logout.innerText = 'Log out';
+
+        //apply to card
+        card.appendChild(img);
+        card.appendChild(h1);
+        card.appendChild(p);
+        card.appendChild(logout);
+
+        //apply eventlistener on logout button
+        logout.onclick = githubLogOut;
+    })
+    .catch(function(err){
+      //Inlogg misslyckades!
+      console.log('Error, inloggning misslyckades!')
+    })
+}
+
+function githubLogOut() {
+  // Logga ut den autentiserade användaren
+  firebase.auth().signOut()
+  .then(function() {
+  	// Utloggning lyckades
+    console.log("User is logged out");
+
+    githubLogin.style.display = 'block';
+    card.style.display = 'none';
+
+    githubLogin();
+
+  })
+  .catch(function(error) {
+    // Utloggning misslyckades
+  	console.log('Error, utloggning misslyckades!')
+  });
 }
 
 
@@ -32,20 +83,6 @@ function github() {
 
 
 
-// Logga ut den autentiserade användaren
-firebase.auth().signOut()
-.then(function(result) {
-	// Utloggning lyckades
-})
-.catch(function(error) {
-	// Utloggning misslyckades
-});
-
-    
-
-
-
-    
 
 
 
@@ -67,7 +104,5 @@ firebase.auth().signOut()
 
 
 
-    
-    
-    
+
 });
